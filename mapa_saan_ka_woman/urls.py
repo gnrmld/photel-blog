6fbe501d3cc57cd8868bc4c15864fe19e6b882d3
Ebok <gn.rmld@gmail.com>
2016-08-01 14:03:13 +0800

    added requirements.txt

--- mapa_saan_ka_woman/urls.py
+++ mapa_saan_ka_woman/urls.py
@@ -6,7 +6,3 @@ from . import views
 urlpatterns = [
     url(r'^$', views.site_location, name='site_location'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
